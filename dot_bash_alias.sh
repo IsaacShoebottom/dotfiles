88e95dde7da6995d1fbbad7977d7d1e4daddc530
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-10-09 09:38:35 -0300

    Update bash aliases, ls function causing issues

--- dot_bash_alias.sh
+++ dot_bash_alias.sh
@@ -1,10 +1,4 @@
-function ls() {
-    if [[ -f .hidden ]]; then 
-        exa --icons --long --ignore-glob="$(cat .hidden | tr '\n' '|')"
-    else
-        exa --icons --long 
-    fi 
-}
+alias ls="exa --icons --long"
 alias grep="ugrep"
 alias cd="z"
 alias cat="bat -p"
